1ed60954f2229dcfcb3851f4814a74ec770cfa5e
OrliSpace <orli.d.p@gmail.com>
2025-06-12 18:52:06 +0300

    move mouse

---
+++ move.py
@@ -0,0 +1,3 @@
+import pyautogui
+
+pyautogui.moveRel(50, 0, duration=0.5)  # יזיז את העכבר 50 פיקסלים ימינה
--- receiver.py
+++ receiver.py
@@ -1,24 +1,19 @@
-# import socket
-
-# # create socket
-# server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# # The post of the ip
-# host = '0.0.0.0'  
-# port = 12345
 
 import socket
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+host = '0.0.0.0'  # מאפשר להתחבר מכל כתובת
+port = 12345
 
-host = os.getenv("SERVER_IP", "0.0.0.0")  # ברירת מחדל: מקבל מכל כתובת
-port = int(os.getenv("SERVER_PORT", "12345"))
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 server_socket.bind((host, port))
 server_socket.listen(1)
+real_ip = socket.gethostbyname(socket.gethostname())
+print(f"Receiver IP (on local network): {real_ip}")
 print(f"Reciever is listening on port {port}...")
+
 
 conn, addr = server_socket.accept()
 print(f"Connected by {addr}")
